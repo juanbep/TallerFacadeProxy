019b855d5c001381b18b33d52e33f1fa0e1a86b1
juanbep <56483953+juanbep@users.noreply.github.com>
2021-04-27 15:59:59 -0500

    versión 1.2

--- src/test/java/co/unicauca/facadeproxy/app/client/OrderFacadeTest.java
+++ src/test/java/co/unicauca/facadeproxy/app/client/OrderFacadeTest.java
@@ -1,13 +1,13 @@
 package co.unicauca.facadeproxy.app.client;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
 import co.unicauca.facadeproxy.domain.order.Customer;
 import co.unicauca.facadeproxy.domain.order.Dish;
 import co.unicauca.facadeproxy.domain.order.OrderFacade;
 import co.unicauca.facadeproxy.domain.order.State;
-import static junit.framework.Assert.assertEquals;
-import org.junit.Test;
-
-
 
 /*
  * Copyright (C) 2021 Luis Tabares
@@ -33,22 +33,23 @@ import org.junit.Test;
  */
 public class OrderFacadeTest {
 
-   @Test
-    public void testCreateOrder() {
-        OrderFacade facade = new OrderFacade();
-        facade.createOrder(new Customer(1, "Carlos Sanchez", "Calle 12 No. 12-12 BarrioCaldas", "3115677899", "Popayán"));
-        facade.addDish(new Dish(1, "Hamburguesa vegetariana", 5000), 2);
-        facade.addDish(new Dish(2, "Hamburguesa sencilla ", 4000), 3);
-        facade.addDish(new Dish(3, "Jugo hit ", 1000), 2);
-        assertEquals("Hamburguesa vegetariana",
-        facade.getOrder().getDetails().get(0).getDish().getName());
-        assertEquals(State.NEW, facade.getOrder().getState());
-        facade.changeState(State.FINALIZED);
-        assertEquals(State.FINALIZED, facade.getOrder().getState());
-        assertEquals(3, facade.totalDishes());
-        assertEquals(24000, facade.calculateTotal());
-        facade.cancelOrder();
-        facade.changeState(State.CANCELLED);
-    }
+	 @Test
+	    public void testCreateOrder() {
+	        OrderFacade facade = new OrderFacade();
+	        facade.createOrder(new Customer(1, "Carlos Sanchez", "Calle 12 No. 12-12 BarrioCaldas", "3115677899", "Popayán"));
+	        facade.addDish(new Dish(1, "Hamburguesa vegetariana", 5000), 2);
+	        facade.addDish(new Dish(2, "Hamburguesa sencilla ", 4000), 3);
+	        facade.addDish(new Dish(3, "Jugo hit ", 1000), 2);
+	        assertEquals("Hamburguesa vegetariana",
+	        facade.getOrder().getDetails().get(0).getDish().getName());
+	        assertEquals(State.NEW, facade.getOrder().getState());
+	        facade.changeState(State.FINALIZED);
+	        assertEquals(State.FINALIZED, facade.getOrder().getState());
+	        assertEquals(3, facade.totalDishes());
+	        assertEquals(24000, facade.calculateTotal());
+	        facade.cancelOrder();
+	        facade.changeState(State.CANCELLED);
+	    }
+
     
 }
